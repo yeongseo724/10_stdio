a1a4ac766d51437fe9ca19f2bf2218f19a8b5216
yeongseo724 <70522995+yeongseo724@users.noreply.github.com>
2020-11-05 15:34:29 +0900

    10 coding(1)

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int i = 10;
+	char c = 69;
+	float f= 12.3;
+	
+	int *ptr;
+	ptr = &i;
+	
+	printf("i: %i %p \n", i, &i);
+	printf("c: %c %p \n", c, &c);
+	printf("f: %f %p \n", f, &f);
+	
+	return 0;
+}
